aa115e5c1200e33051332af51d3be50bbb721011
706com <ehdgusjdbswl@gmail.com>
2024-04-12 19:53:47 +0900

    feat : 상품 변경 시간내역 관리 추상 entity 생성

---
+++ src/main/java/sample/cafekiosk/spring/domain/BaseEntity.java
@@ -0,0 +1,24 @@
+package sample.cafekiosk.spring.domain;
+
+import java.time.LocalDateTime;
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+//엔티티가 자동으로 생성되거나 변경됐을 때마다 시간이 찍힌다.
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseEntity {
+
+    @CreatedDate
+    private LocalDateTime createdDateTime;
+
+    @LastModifiedDate
+    private LocalDateTime modifiedDateTime;
+}
+
