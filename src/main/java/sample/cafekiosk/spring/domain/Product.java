bc672d64cce9c1fed225636a452c36880084af35
706com <ehdgusjdbswl@gmail.com>
2024-04-13 19:29:10 +0900

    add : 생성자 추가

--- src/main/java/sample/cafekiosk/spring/domain/Product.java
+++ src/main/java/sample/cafekiosk/spring/domain/Product.java
@@ -7,6 +7,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -25,9 +26,18 @@ public class Product extends BaseEntity{
     private ProductType type;
 
     @Enumerated(EnumType.STRING)
-    private ProductSellingType sellingType;
+    private ProductSellingStatus sellingStatus;
 
     private String name;
 
     private int price;
+
+    @Builder    // id 생성은 DB에게 위임했기에 생략
+    private Product(String productNumber, ProductType type, ProductSellingStatus sellingStatus, String name, int price) {
+        this.productNumber = productNumber;
+        this.type = type;
+        this.sellingStatus = sellingStatus;
+        this.name = name;
+        this.price = price;
+    }
 }
